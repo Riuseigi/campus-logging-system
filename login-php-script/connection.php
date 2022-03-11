571a82a220684d496cb839f54c8ce41f2d59c823
Troy Liam Jarata <troyliamjarata@gmail.com>
2022-03-11 19:51:55 +0800

    Update connection.php
    connecting to current database

--- login-php-script/connection.php
+++ login-php-script/connection.php
@@ -1,3 +1,15 @@
 <?php 
-$con = mysqli_connect('localhost', 'root', '', 'campus');
+/*
+Server: sql6.freemysqlhosting.net
+Name: sql6477119
+Username: sql6477119
+Password: 2vCwuT3g4f
+Port number: 3306
+*/
+$servername = "sql6.freemysqlhosting.net";
+$username = "sql6477119";
+$password = "2vCwuT3g4f";
+$dbname = "sql6477119";
+
+$con = mysqli_connect($servername, $username, $password, $dbname);
 ?>
